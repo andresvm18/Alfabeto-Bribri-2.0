7d70075f72c6a93a00fd61fcdb2cbae9865603a0
andresvm18 <viquezandres1811@gmail.com>
2025-07-28 19:08:53 -0600

    Corrección de rutas
    Co-Authored-By: paula <117476611+palita2503@users.noreply.github.com>

--- AlfabetoBribri/src/App.jsx
+++ AlfabetoBribri/src/App.jsx
@@ -4,6 +4,8 @@ import Home from "./Shared/Screens/Home";
 import Alphabet from "./Alphabet/Screens/Alphabet"
 import Character from "./Alphabet/Screens/Character";
 import AFIEquivalencies from "./Alphabet/Screens/AFIEquivalencies";
+import Game from "./Game/Screens/GameHome";
+import GameMode from "./Game/Screens/Game";
 import History from "./Shared/Screens/History";
 import Footer from "./Shared/Components/Footer";
 
@@ -17,6 +19,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/alfabeto" element={<Alphabet />} />
           <Route path="/caracter/:letra" element={<Character />} />
+           <Route path="/aprende" element={<Game />} />
+          <Route path="/juego/:gameMode" element={<GameMode />} />
           <Route path="/equivalencias-afi" element={<AFIEquivalencies />} />
           <Route path="/historia" element={<History />} />
         </Routes>
